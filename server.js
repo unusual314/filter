0103b77e5b3a474fcd54677303ca654b1012dd90
unusual314 <125853873+unusual314@users.noreply.github.com>
2026-02-18 13:55:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,28 +3,12 @@ const axios = require('axios');
 const app = express();
 app.get('/pi', async (req, res) => {
   const response = await axios.get('https://www.zztv.xyz/api/tvbox/subscribe?token=31415926&adFilter=true'); 
-  let data = response.data;
-  if (typeof data === 'string') {
-    data = JSON.parse(data);
-  }
-  // 只过滤sites中包含🔞的项目
-  if (data.sites && Array.isArray(data.sites)) {  
-    data.sites = data.sites.filter(item => !item.name || !item.name.includes('🔞'));
-  }
   res.set('Content-Type', 'application/json');
-  res.json(data);
+  res.send(response.data);
 });
 app.get('/bi', async (req, res) => {
   const response = await axios.get('https://www.zztv.xyz/api/tvbox/subscribe?token=31415926&adFilter=true'); 
-  let data = response.data;
-  if (typeof data === 'string') {
-    data = JSON.parse(data);
-  }    
-  // 只过滤sites中包含🔞的项目
-  if (data.sites && Array.isArray(data.sites)) {
-    data.sites = data.sites.filter(item => !item.name || !item.name.includes('🔞'));
-  }
   res.set('Content-Type', 'application/json');
-  res.json(data);
+  res.send(response.data);
 });
 app.listen(8080, '0.0.0.0');
