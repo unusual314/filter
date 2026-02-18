352ca553d03308547ce02c7a6b3fe795372909d2
unusual314 <125853873+unusual314@users.noreply.github.com>
2026-02-18 13:35:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,12 @@ const axios = require('axios');
 const app = express();
 app.get('/pi', async (req, res) => {
   const response = await axios.get('https://www.zztv.xyz/api/tvbox/subscribe?token=31415926&adFilter=true'); 
-  let data = response.data;
-  if (typeof data === 'string') data = JSON.parse(data);
-  // 只过滤成人内容，不改变其他数据
-  if (data.sites) {
-    data.sites = data.sites.filter(item => !item.name.includes('🔞'));
-  }
-  res.json(data);
+  res.set('Content-Type', 'application/json');
+  res.send(response.data);
 });
 app.get('/bi', async (req, res) => {
   const response = await axios.get('https://www.zztv.xyz/api/tvbox/subscribe?token=31415926&adFilter=true'); 
-  let data = response.data;
-  if (typeof data === 'string') data = JSON.parse(data);
-  // 只过滤成人内容，不改变其他数据
-  if (data.sites) {
-    data.sites = data.sites.filter(item => !item.name.includes('🔞'));
-  }    
-  res.json(data);
+  res.set('Content-Type', 'application/json');
+  res.send(response.data);
 });
 app.listen(8080, '0.0.0.0');
